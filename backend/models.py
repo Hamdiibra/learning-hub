3758bd1f0e6cba8e0bcdba1b31cb30c3e4ba11c9
Hamdiibrahim <yusufhamdi137@gmail.com>
2025-01-27 14:34:18 +0300

    .created Enrollment model

--- backend/models.py
+++ backend/models.py
@@ -16,3 +16,12 @@ class Course(db.Model):
     description = db.Column(db.Text,nullable=True)
     enrolled_courses = db.relationship('Enrollment',back_populates='course' )
 
+#Enrollment Model
+class Enrollment(db.Model):
+    id = db.Column(db.Integer,primary_Key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+    course_id = db.Colun(db.Integer,db.ForeignKey('course.id'))
+    progress = db.Column(db.String(20), default="Not Started")
+
+    user = db.relationship('User', back_populates='enrolled_courses')
+    course = db.relationship('Course',back_populates='enrollments')
