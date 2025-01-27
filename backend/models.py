07e8404c171dfcdd9d5d833521d6f022f19c1f0f
Hamdiibrahim <yusufhamdi137@gmail.com>
2025-01-27 14:33:50 +0300

    .created Course model

--- backend/models.py
+++ backend/models.py
@@ -8,4 +8,11 @@ class User(db.Model):
     username = db.Column(db.String(50), nullable=False)
     enrolled_courses = db.relationship('Enrollment',back_populates='user')
 
+#Course model
+class Course(db.Model):
+    id = db.Column(db.Integer,primary_Key=True)
+    name = db.Column(db.String(100), nullable=True)
+    instructor = db.Column(db.String(50), nullable=True)
+    description = db.Column(db.Text,nullable=True)
+    enrolled_courses = db.relationship('Enrollment',back_populates='course' )
 
