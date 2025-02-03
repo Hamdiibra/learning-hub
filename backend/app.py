cbf9a00c87112ed9618f8b7380ea6e0510402205
Hamdiibrahim <yusufhamdi137@gmail.com>
2025-02-03 07:36:56 +0300

    app.py

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -9,7 +8,7 @@ from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3')
+app.config['SQLALCHEMY_DATABASE_URI'] =  'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = 'your_secret_key'  # Use a real secret key
 jwt = JWTManager(app)
